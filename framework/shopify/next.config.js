dbff31ca7a6939a6d28b8e3e4672fd0aeab0c9f3
juliscapucin <jscapucin@gmail.com>
2022-07-06 17:14:22 +0200

    customizable next.config for different frameworks

---
+++ framework/shopify/next.config.js
@@ -0,0 +1,1 @@
+module.exports = { test: "shopify unique settings" };
--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["unsplash.com", "next.config.js"],
-  },
-};
 
-module.exports = nextConfig;
+const { withFrameworkConfig } = require("./framework/common/config");
+
+module.exports = withFrameworkConfig({
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+});
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
+
+// Original / default config
+// module.exports = nextConfig;
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["unsplash.com", "next.config.js"],
+//   },
+// };
